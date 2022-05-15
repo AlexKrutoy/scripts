562888297db1752b47b373f51834fc173b5d6409
AlexKrut <65369825+AlexKrutoy@users.noreply.github.com>
2022-05-16 02:15:47 +0300

    Create Blyat.py

---
+++ Blyat.py
@@ -0,0 +1,14 @@
+import logging
+
+from .. import loader, utils
+
+@loader.tds
+class ChirovMod(loader.Module):
+	"""Слава украине на каждое сообщение чирова"""
+	strings = {
+        'name': 'Chirov'}
+        
+        async def chirovcmd(self, message):
+        	"""Kekw"""
+        logger.debug("logged")
+        await utils.answer(message, "да")
